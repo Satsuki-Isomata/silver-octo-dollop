09f1f04dbd013ffe1a7c961f9b343c18a8280e9b
isomata <isomata-s@aeonpeople.biz>
2018-02-08 13:25:43 +0900

    modify:変数でmodeを切り替え

--- gulpfile.js
+++ gulpfile.js
@@ -20,47 +20,74 @@ svg-sprite
  */
 
 // sprite設定
-config = {
-mode: {
-  // cssでsvg指定
-  view: {
-    render: {
-      scss: {
-        dest: '../src/sass/sprite/_svgSprite.scss',// gulpfileから見たsass出力ディレクトリ
+view = {
+  mode: {
+    // cssでsvg指定
+    view: {
+      render: {
+        scss: {
+          dest: '../src/sass/sprite/_svgSprite.scss',// gulpfileから見たsass出力ディレクトリ
+        },
       },
-    },
-    sprite: '../assets/img/sprite/sprite.view.svg',// gulpfileから見たsvg出力場所(指定しないと/view/ができる)
-    dimensions: false, // sass上でのwidth/height指定を削除
-    bust: false // キャッシュ用パラメータを削除
-  },
-
-  // useタグでsvg指定
-  symbol: {
-    sprite: '../assets/img/sprite/sprite.symbol.svg',// gulpfileから見たsvg出力場所
-  }
-},
-shape : {
-  transform: [
-    {
-      svgo: { // svgoオプション
-        plugins: [
-          { 'removeTitle': true }, // titleを削除
-          { 'removeStyleElement': true }, // styleを削除
-          { 'removeAttrs': { 'attrs': 'fill' } }, // fillを削除
-          { 'removeXMLNS': true }, // xmlnを削除
-          { 'removeDimensions': true } // width/heightを削除
-        ]
-      }
+      sprite: '../assets/img/sprite/sprite.view.svg',// gulpfileから見たsvg出力場所(指定しないと/view/ができる)
+      dimensions: false, // sass上でのwidth/height指定を削除
+      bust: false // キャッシュ用パラメータを削除
     }
-  ]
-},
-svg : {
-  xmlDeclaration: false // xml宣言を削除
-}
+  },
+  shape : {
+    transform: [
+      {
+        svgo: { // svgoオプション
+          plugins: [
+            { 'removeTitle': true }, // titleを削除
+            { 'removeXMLNS': true }, // xmlnを削除
+            { 'removeDimensions': true } // width/heightを削除
+          ]
+        }
+      }
+    ]
+  },
+  svg : {
+    xmlDeclaration: false // xml宣言を削除
+  }
 };
+symbol = {
+  mode: {
+    // useタグでsvg指定
+    symbol: {
+      sprite: '../assets/img/sprite/sprite.symbol.svg',// gulpfileから見たsvg出力場所
+    }
+  },
+  shape : {
+    transform: [
+      {
+        svgo: { // svgoオプション
+          plugins: [
+            { 'removeTitle': true }, // titleを削除
+            { 'removeStyleElement': true }, // styleを削除
+            { 'removeAttrs': { 'attrs': 'fill' } }, // fillを削除
+            { 'removeXMLNS': true }, // xmlnを削除
+            { 'removeDimensions': true } // width/heightを削除
+          ]
+        }
+      }
+    ]
+  },
+  svg : {
+    xmlDeclaration: false // xml宣言を削除
+  }
+};
+
 gulp.task('svg-sprite', function() {
+
+  // symbol mode
   gulp.src('assets/img/*.svg')
-  .pipe(svgSprite(config))
+  .pipe(svgSprite(symbol))
+  .pipe(gulp.dest('.'));
+
+  // view mode
+  gulp.src('assets/img/*.svg')
+  .pipe(svgSprite(view))
   .pipe(gulp.dest('.'));
 });
 
